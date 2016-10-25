edff2a60e59cda97233b1f0a95822d15360163b4
Ozgur Tuzgen <ozgurtuzgen@hotmail.com>
2016-10-26 00:59:26 +0300

    routing done

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,39 +1,28 @@
-import {NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule}   from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import {AppComponent} from './app.component'
-import {HeroesComponent} from './heroes.component';
-import {HeroDetailComponent} from './hero-detail.component';
-import {HeroService} from './hero.service';
-import {DashboardComponent} from './dashboard.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-    imports: [ BrowserModule,FormsModule, 
-    RouterModule.forRoot([
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            }
-        ])
-    ],
-    exports: [],
-    declarations: [AppComponent, HeroesComponent,HeroDetailComponent,DashboardComponent],
-    bootstrap: [AppComponent],
-    providers: [HeroService]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
