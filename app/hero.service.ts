6a2f826243e0cbc9b7491bde7e1b681b6895d335
Özgür TÜZGEN <otuzgen@stm.com.tr>
2016-10-26 18:38:26 +0300

    next step: delete a hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,27 +1,49 @@
 import { Injectable } from '@angular/core';
-import {Hero} from './hero';
-import {HEROES} from './heroes';
+import { Http, Headers } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
 
-    constructor() { }
+    private heroesUrl = 'app/heroes';
 
-    getHeroes():Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+
+    constructor(private http: Http) { }
+
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
 
-    getHeroesSlowly():Promise<Hero[]>{
-        return new Promise<Hero[]>(resolve=> 
-            setTimeout(resolve,2000)).then(()=>
-            this.getHeroes());
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
     }
 
-    getTopHeroes():Promise<Hero[]>{
-        return Promise.resolve(HEROES.slice(1,5));
+    update(hero: Hero): Promise<Hero> {
+        const updateUrl = `${this.heroesUrl}/${hero.id}`;
+        return this.http.put(updateUrl, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
     }
 
-    getHero(id:number):Promise<Hero>{
-        return this.getHeroes().then(heroes => heroes.find(hero=>hero.id === id));
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     }
 }
